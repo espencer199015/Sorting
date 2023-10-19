4cb40ad8303bed61404be187c86b0ed3fb3dc450
Liz <elizabeth.erlandson1@gmail.com>
2023-10-18 22:13:58 -0400

    first push sorting exercise

---
+++ quick.js
@@ -0,0 +1,25 @@
+function pivot(arr, start = 0, end = arr.length - 1) {
+  const pivotValue = arr[start];
+  let pivotIndex = start;
+
+  for (let i = start + 1; i <= end; i++) {
+    if (arr[i] < pivotValue) {
+      pivotIndex++;
+      [arr[i], arr[pivotIndex]] = [arr[pivotIndex], arr[i]];
+    }
+  }
+
+  [arr[start], arr[pivotIndex]] = [arr[pivotIndex], arr[start]];
+  return pivotIndex;
+}
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    const pivotIndex = pivot(arr, left, right);
+    quickSort(arr, left, pivotIndex - 1);
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
+
+module.exports = quickSort;
---
+++ radix.js
@@ -0,0 +1,31 @@
+function getDigit(num, place) {
+  return Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;
+}
+
+function digitCount(num) {
+  if (num === 0) return 1;
+  return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+
+function mostDigits(arr) {
+  let maxDigits = 0;
+  for (let i = 0; i < arr.length; i++) {
+    maxDigits = Math.max(maxDigits, digitCount(arr[i]));
+  }
+  return maxDigits;
+}
+
+function radixSort(arr) {
+  const maxDigitCount = mostDigits(arr);
+  for (let k = 0; k < maxDigitCount; k++) {
+    const buckets = Array.from({ length: 10 }, () => []);
+    for (let i = 0; i < arr.length; i++) {
+      const digit = getDigit(arr[i], k);
+      buckets[digit].push(arr[i]);
+    }
+    arr = [].concat(...buckets);
+  }
+  return arr;
+}
+
+module.exports = radixSort;
